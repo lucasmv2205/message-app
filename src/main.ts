14eab98b432f7365cbf92dbb8cfe8570733d19ec
lucasmv2205 <lmartins2205@gmail.com>
2022-06-26 21:07:14 -0300

    enable cots

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ async function bootstrap() {
 
   app.setGlobalPrefix('/api/v1');
   app.useGlobalPipes(new ValidationPipe());
+  app.enableCors();
 
   await app.listen(process.env.PORT || 8080);
 }
